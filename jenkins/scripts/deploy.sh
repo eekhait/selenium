da8a1627f2049d3cc08839650b63fc6e7236c3c0
Khai <eekhai.t@gmail.com>
2021-11-14 16:45:48 +0800

    Update deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 80:80 --name my-apache-php-app -v c:\\...\\selenium\\src:/var/www/html php:7.2-apache
+docker run -d -p 80:80 --name my-apache-php-app -v C:\Users\dOnNy\Desktop\SIT\Trimester_6\ICT3203\Lab 7B\selenium\\src:/var/www/html php:7.2-apache
 sleep 1
 set +x
 
